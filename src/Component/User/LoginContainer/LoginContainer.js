1e9cad3945072d6db69c19495cb2670975108ba3
kyupkyup <rudduqdl@naver.com>
2021-05-28 23:24:53 +0900

    refactoring: login modal component 분리

---
+++ src/Component/User/LoginContainer/LoginContainer.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { UserContext } from "../../../App";
+import Modal from "react-modal";
+import Login from "../Login/Login";
+import SignIn from "../SignIn/SignIn";
+Modal.setAppElement("#root");
+
+const LoginContainer = () => {
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      border: "none",
+      boxShadow: "0 4px 6px 0 rgb(20 20 20 / 40%)",
+    },
+  };
+  const user = useContext(UserContext);
+  return (
+    <Modal
+      isOpen={user.modalIsOpen}
+      // onAfterOpen={afterOpenModal}
+      onRequestClose={user.closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      {user.modalState === "로그인" ? (
+        <Login
+          closeModal={user.closeModal}
+          setModalSignIn={user.setModalSignIn}
+        />
+      ) : (
+        <SignIn
+          closeModal={user.closeModal}
+          setModalLogIn={user.setModalLogIn}
+        />
+      )}
+    </Modal>
+  );
+};
+export default LoginContainer;
